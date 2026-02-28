50cc9a4ab5dc952efad87304f5c38e420ca12d85
burgesslivee <burgesslivee@gmail.com>
2026-02-27 19:42:24 -0800

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import BirthChartPath from './BirthChartPath.jsx'
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BirthChartPath />
+  </React.StrictMode>,
+)
